cad340a4b4788caf69e442f40f42c88f6cd5de85
wai1 <walt.iddings@gmail.com>
2024-03-25 16:33:34 -0400

    Added parenthesis to the print statements.

--- print_messages.py
+++ print_messages.py
@@ -1,3 +1,3 @@
-print "This is a new branch."
-print "Text will be outputted to the terminal."
-print "That is all."
+print("This is a new branch.")
+print("Text will be outputted to the terminal.")
+print("That is all.")
